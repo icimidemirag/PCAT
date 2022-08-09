49c775290e14e6a80904c084cadbdd2c25643898
İçimi Demirağ <icimidemirag@gmail.com>
2022-08-09 13:25:19 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+//MIDDLEWARES
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
